d503691d704fd6a94060a99c7953623e40195f04
Adria Navarro <anavarromartin@pivotal.io>
2019-04-19 15:11:49 -0500

    Added application

---
+++ src/tennis/TennisApplication.java
@@ -0,0 +1,33 @@
+package tennis;
+
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
+public class TennisApplication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TennisGame game = new TennisGame();
+        while (true) {
+            try {
+                System.out.println("Who scores? (P1 | P2): ");
+                String player = sc.nextLine();
+                if (player.equals("P1")) {
+                    game.wonPoint(Player.ONE);
+                } else if (player.equals("P2")) {
+                    game.wonPoint(Player.TWO);
+                } else {
+                    throw new InvalidParameterException();
+                }
+
+                String score = game.getScore();
+                System.out.println("Score: " + score);
+                if (score.toLowerCase().contains("wins!")) {
+                    break;
+                }
+            } catch (InvalidParameterException e) {
+                System.out.println("Error: invalid player.");
+            }
+        }
+    }
+}
--- src/tennis/TennisGameTest.java
+++ src/tennis/TennisGameTest.java
@@ -1,11 +1,9 @@
 package tennis;
 
-import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 class TennisGameTest {
 
