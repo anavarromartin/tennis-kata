be439e608a8f4061d693f08e1bc12bcd1119d857
Adria Navarro <anavarromartin@pivotal.io>
2019-04-19 14:16:46 -0500

    initial commit:

---
+++ src/tennis/TennisGame.java
@@ -0,0 +1,39 @@
+package tennis;
+
+/*
+1. A game is won by the first player to have won at least 4
+points in total and at least 2 points more than the opponent.
+The score is then "Win for player1" or "Win for player2"
+
+2. The running score of each game is described in a manner
+peculiar to Tennis. Scores are described as:
+
+ 0: "Love"
+ 1: "Fifteen"
+ 2: "Thirty"
+ 3: "Forty"
+
+3. If at least three points have been scored by
+each player and the scores are equal,
+the score is "Deuce".
+
+4. If at least three points have been scored
+by each side, and a player has more points than
+his or her opponent the score of the game is
+"Advantage player1" or "Advantage player2"
+
+ */
+public class TennisGame {
+
+    public String getScore() {
+       return null;
+    }
+
+    public void wonPoint(Player player) {
+
+    }
+}
+
+enum Player {
+    ONE, TWO
+}
---
+++ src/tennis/TennisGameTest.java
@@ -0,0 +1,17 @@
+package tennis;
+
+import org.junit.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+class TennisGameTest {
+
+    @Test
+    @DisplayName("when p1 scores, returns {Fifteen, Love}")
+    void P1_scores__shouldReturn_() {
+        assertTrue(false);
+    }
+}
