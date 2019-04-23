056fa7b0ed75417db9eb79d69c2d2d745147d4c1
Pivotal <pivotal@iMac-4.local>
2019-04-23 09:40:54 -0500

    added cheatsheet

--- src/tennis/TennisGameTest.java
+++ src/tennis/TennisGameTest.java
@@ -5,6 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+/*
+    CHEAT SHEET
+
+        - To create a new test:
+            type 'test', and press enter
+
+        - Assertions guide:
+            assertTrue( <x> ): will fail if <x> is false
+            assertFalse( <x> ): will fail if <x> is true
+            assertEquals(<expected>, <actual>): will fail if <expected> != <actual>
+
+        - TDD flow:
+            1. Write the minimal amount of test that will fail
+            2. Write the minimal amount of code that will pass the test
+            3. Refactor
+            4. Go back to (1)
+*/
+
 class TennisGameTest {
 
     @Test
