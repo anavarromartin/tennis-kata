aa249181175cebf9f3369ba644b8179ee3696ce1
Pivotal <pivotal@iMac-4.local>
2019-04-23 09:06:02 -0500

    added junit5 into classspath

--- src/tennis/TennisGameTest.java
+++ src/tennis/TennisGameTest.java
@@ -3,14 +3,13 @@ package tennis;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class TennisGameTest {
 
     @Test
     @DisplayName("when p1 scores, returns {Fifteen, Love}")
     void P1Scores() {
-        assertTrue(false);
+        fail();
     }
 }
